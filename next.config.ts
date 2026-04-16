80318e7ad8df19d923fedee62bbcfb59c9044d7c
GersonNovaWeb <hello@novaweb-agency.com>
2026-04-16 14:48:48 -0600

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production';
+import type { NextConfig } from 'next';
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: 'export',
-
-  basePath: isProd ? '/Report_MHOS' : '',
-  assetPrefix: isProd ? '/Report_MHOS/' : '',
-  images: {
-    unoptimized: true,
-  },
+  basePath: '/Report_MHOS',
+  assetPrefix: '/Report_MHOS/',
 };
 
 export default nextConfig;
